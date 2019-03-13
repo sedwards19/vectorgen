d6ab57abfe964dd7015503c44377f96fe67a8ad3
Claire Burnett <claireburnett@Claire--Trevor-iMac.local>
2019-03-13 13:48:13 -0600

    Add structure to page-research.php

---
+++ page-research.php
@@ -0,0 +1,68 @@
+<?php /*Template Name: Research Page Template*/?>
+
+<?php get_header(); ?>
+
+<div class="container">
+    
+<!--Hero Image Section-->
+    <div class="row">
+        <div class="twelve columns">
+            <?php dynamic_sidebar('research-hero-image'); ?>
+        </div>
+    </div>
+
+<!--Global Crisis Section-->
+    <div class="row">
+        <div class="twelve columns">
+            <?php dynamic_sidebar('global-crisis'); ?>
+        </div>
+    </div>
+
+<!--Icon Section-->
+    <div class="row">
+        <div class="twelve columns">
+            <?php dynamic_sidebar('research-icon-one'); ?>
+        </div>
+
+        <div class="twelve columns">
+            <?php dynamic_sidebar('research-icon-two'); ?>
+        </div>
+
+        <div class="twelve columns">
+            <?php dynamic_sidebar('research-icon-three'); ?>
+        </div>
+        
+        <div class="twelve columns">
+            <?php dynamic_sidebar('research-icon-four'); ?>
+        </div>
+    </div>
+    
+<!--Large Image + Content Grid Section-->
+    <div class="row">
+        <div class="twelve columns">
+            <?php dynamic_sidebar('research-large-image-one'); ?>
+        </div>
+
+        <div class="twelve columns">
+            <?php dynamic_sidebar('research-combating-hiv'); ?>
+        </div>
+
+        <div class="twelve columns">
+            <?php dynamic_sidebar('research-large-image-two'); ?>
+        </div>
+        
+        <div class="twelve columns">
+            <?php dynamic_sidebar('research-vectorgen-approach'); ?>
+        </div>
+    </div>
+
+<!--Future of Vectors and Gene Therapy Section-->
+    <div class="row">
+        <div class="twelve columns">
+            <?php dynamic_sidebar('future'); ?>
+        </div>
+    </div>
+
+</div>
+
+<?php get_footer(); ?>
