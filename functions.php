f1a35ade1fbf18191bfd147b352bb74967887b2c
Sydney Edwards <sydneyedwards@sydneys-air.attlocal.net>
2019-03-12 20:29:21 -0400

    Remove functions

--- functions.php
+++ functions.php
@@ -1,16 +1,1 @@
-<?php
-//Add custom menus
-function custom_menus(){
-  register_nav_menus( array (
-    'header-menu' => ('Header Menu'),
-    'footer-menu' => ('Footer Menu')
-  ));
-}
-add_action('init', 'custom_menus');
 
-//Add Custom Logo
-add_theme_support( 'custom-logo' );
-
-//Adds widget areas
-
-?>
