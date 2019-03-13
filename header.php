a2b920baf05d86aa9dad88ac0b5d763a6b6baa47
Sydney Edwards <sydneyedwards@sydneys-air.attlocal.net>
2019-03-12 21:09:33 -0400

    Add Header Image Start

--- header.php
+++ header.php
@@ -6,8 +6,35 @@
 
     <!-- Link to our style.css file -->
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
-    
+
     <?php wp_head(); ?>
 </head>
-    
+
 <body>
+  <header>
+    <div class="container">
+     <div class="row">
+       <div class="three columns">
+         <?php if(get_header_image() -- ''){?>
+          <h1><a href="<?php $url = home_url('/'); echo $url; ?>"><?php bloginfo('name'); ?></a></h1><?php
+        } ?>
+
+
+
+
+
+
+
+
+
+        
+       </div>
+
+       <div class="nine columns">
+         <?php wp_nav_menu(array (
+           'theme_location' => 'header-menu',
+           'container-class' => 'menu-header'
+         ));
+         ?>
+     </div>
+</header>
