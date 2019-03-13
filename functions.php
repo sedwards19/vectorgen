7914917e6040fc0094b78aa567149711b261f5b9
Sydney Edwards <sydneyedwards@sydneys-air.attlocal.net>
2019-03-12 22:02:06 -0400

    Add widgets

--- functions.php
+++ functions.php
@@ -11,6 +11,62 @@ function blank_widgets_init(){
     'before_title'  => '<h3 class="widget-title">',
     'after_title'   => '</h3>'
   ));
+
+  //Home-Hero Image Widget
+  register_sidebar( array(
+    'name'          => ('Hero Image'),
+    'id'            => 'hero-image',
+    'description'   => 'Hero Image on Home Page',
+    'before_widget' => '<div class="hero-image-widget">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3 class="widget-title">',
+    'after_title'   => '</h3>'
+  ));
+
+  //Home-The Vector Gen Approach Text Block Widget
+  register_sidebar( array(
+    'name'          => ('Vector Gen Approach Text Block'),
+    'id'            => 'vector-gen-approach',
+    'description'   => 'Widget Area for Vector Gen Approach Section',
+    'before_widget' => '<div class="vector-gen-approach">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3 class="widget-title">',
+    'after_title'   => '</h3>'
+  ));
+
+  //Home-Left Topic Widget
+  register_sidebar( array(
+    'name'          => ('Left Topic Home'),
+    'id'            => 'left-topic-home',
+    'description'   => 'Widget Area In The Left Topic Home Page',
+    'before_widget' => '<div class="left-topic-home">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3 class="widget-title">',
+    'after_title'   => '</h3>'
+  ));
+
+  //Home-Middle Topic Widget
+  register_sidebar( array(
+    'name'          => ('Middle Topic Home'),
+    'id'            => 'middle-topic-home',
+    'description'   => 'Widget Area In The Middle Topic Home Page',
+    'before_widget' => '<div class="middle-topic-home">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3 class="widget-title">',
+    'after_title'   => '</h3>'
+  ));
+
+  //Home-Right Topic Widget
+  register_sidebar( array(
+    'name'          => ('Right Topic Home'),
+    'id'            => 'right-topic-home',
+    'description'   => 'Widget Area In The Right Topic Home Page',
+    'before_widget' => '<div class="right-topic-home">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3 class="widget-title">',
+    'after_title'   => '</h3>'
+  ));
+
 }
 
 add_action('widgets_init', 'blank_widgets_init');
