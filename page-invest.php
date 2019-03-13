93d91bc052efa092814a9eb3c968124f8c807312
Claire Burnett <claireburnett@Claire--Trevor-iMac.local>
2019-03-13 13:00:04 -0600

    Finish structuring page-invest.php

--- page-invest.php
+++ page-invest.php
@@ -1,7 +1,9 @@
 <?php /*Template Name: Invest Page Template*/?>
+
 <?php get_header(); ?>
 
 <div class="container">
+    
 <!--Hero Image Section-->
     <div class="row">
         <div class="twelve columns">
@@ -18,27 +20,39 @@
 
 <!--Investment Potential Section-->
     <div class="row">
-        <div class="one-third column">
+        <div class="twelve columns">
             <?php dynamic_sidebar('invest-left-icon'); ?>
         </div>
 
-        <div class="one-third column">
+        <div class="twelve columns">
             <?php dynamic_sidebar('invest-middle-icon'); ?>
         </div>
 
-        <div class="one-third column">
+        <div class="twelve columns">
             <?php dynamic_sidebar('invest-right-icon'); ?>
         </div>
     </div>
 
-<!--Newsletter Sign Up Section-->
+<!--Budget & Timeline Section-->
     <div class="row">
         <div class="twelve columns">
-            <?php dynamic_sidebar('newsletter-sign-up'); ?>
+            <?php dynamic_sidebar('budget-timelime'); ?>
         </div>
     </div>
 
-<!--Logos-->
+<!--Returns & Projections Section-->
+    <div class="row">
+        <div class="twelve columns">
+            <?php dynamic_sidebar('returns-projections'); ?>
+        </div>
+    </div>
+    
+<!--Become an Investor Section-->
+    <div class="row">
+        <div class="twelve columns">
+            <?php dynamic_sidebar('become-investor'); ?>
+        </div>
+    </div>
 
 </div>
 
