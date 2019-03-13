0d6499b6d1934cc6ba909059f59a02d39460c211
Sydney Edwards <sydneyedwards@sydneys-air.attlocal.net>
2019-03-12 20:41:05 -0400

    Update functions file

--- functions.php
+++ functions.php
@@ -1,1 +1,17 @@
+<?php
+//Adds widget areas
+function blank_widgets_init(){
+  //Header Widget
+  register_sidebar( array(
+    'name'          => ('Header Right'),
+    'id'            => 'header-right',
+    'description'   => 'Right widget area in the header',
+    'before_widget' => '<div class="widget-header widget-right">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3 class="widget-title">',
+    'after_title'   => '</h3>'
+  ));
+}
 
+add_action('widgets_init', 'blank_widgets_init');
+?>
