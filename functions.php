917edd2ea01df6881309019a0fb08ff18fe7f9df
Sydney Edwards <sydneyedwards@sydneys-air.attlocal.net>
2019-03-12 22:17:34 -0400

    Add footer widgets

--- functions.php
+++ functions.php
@@ -122,7 +122,38 @@ function blank_widgets_init(){
     'after_title'   => '</h3>'
   ));
 
+  //Left Footer Widget
+  register_sidebar( array(
+    'name'          => ('Left Footer'),
+    'id'            => 'left-footer',
+    'description'   => 'Left widget area in the footer',
+    'before_widget' => '<div class="widget-footer widget-left">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3 class="widget-title">',
+    'after_title'   => '</h3>'
+  ));
 
+  //Middle Footer Widget
+  register_sidebar( array(
+    'name'          => ('Middle Footer'),
+    'id'            => 'middle-footer',
+    'description'   => 'Middle widget area in the footer',
+    'before_widget' => '<div class="widget-footer widget-middle">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3 class="widget-title">',
+    'after_title'   => '</h3>'
+  ));
+
+  //Right Footer Widget
+  register_sidebar( array(
+    'name'          => ('Right Footer'),
+    'id'            => 'right-footer',
+    'description'   => 'Right widget area in the footer',
+    'before_widget' => '<div class="widget-footer widget-right">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3 class="widget-title">',
+    'after_title'   => '</h3>'
+  ));
 }
 
 add_action('widgets_init', 'blank_widgets_init');
