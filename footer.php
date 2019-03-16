d391c3ecb0bce59b1e80ccabd51e4a1168ff287c
Sydney Edwards <sydneyedwards@Sydneys-Air-2.lan>
2019-03-16 14:47:12 -0400

    Update footer layout

--- footer.php
+++ footer.php
@@ -1,3 +1,24 @@
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="one-third column">
+        <?php dynamic_sidebar('left-footer');?>
+      </div>
+
+      <div class="one-third column">
+        <?php dynamic_sidebar('middle-footer');?>
+      </div>
+
+      <div class="one-third column">
+        <?php wp_nav_menu(array (
+          'theme_location' => 'footer-menu',
+          'container-class' => 'menu-footer'
+        ));
+        ?>
+      </div>
+    </div>
+  </div>
+</footer>
 <?php wp_footer(); ?>
 </body>
 </html>
