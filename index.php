5f13c994c7584bafa92242a4d2ed090cc59bfc37
Rachel McCart <rlmccart@ufl.edu>
2019-03-16 07:54:01 -0500

    Template name to index.php and call hero image widget.

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php get_header(); ?>
+<?php /*Template Name: Main Blog Page - Resource Center*/
+
+get_header(); ?>
+
+<div class="container">
+<!--Hero Image Section-->
+  <div class="row">
+    <div class="twelve columns">
+      <?php dynamic_sidebar('resource-hero-image'); ?>
+    </div>
+  </div>
+
 
 <?php get_footer(); ?>
