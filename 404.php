25875b67b3a940a001a5eff08d60f4efd4b7b8ae
Claire Burnett <claireburnett@Claire--Trevor-iMac.local>
2019-03-16 12:24:39 -0600

    Update body copy on 404 page

--- 404.php
+++ 404.php
@@ -6,8 +6,8 @@ get_header(); ?>
     <div class="row">
         <div class="twelve columns not-found">
             <h2>Page not found</h2>
-            <h3>This usually never happens</h3>
-            <p>It looks like nothing was found at this location. Would you like to go back <a href="<?php echo home_url('/'); ?>">home</a>?</p>
+            <h3>Something's amiss, just like a proper cure for HIV</h3>
+            <p>Join us in rewriting the story. Head on back to the <a href="<?php echo home_url('/'); ?>">home</a>page to learn more.</p>
         </div>
     </div>
 </div>
