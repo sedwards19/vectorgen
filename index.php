385337d85ac87c025dd3f59636b86c192904b470
Rachel McCart <rlmccart@ufl.edu>
2019-03-16 10:07:40 -0500

    Add if have posts loop to display recent posts on Resource Page.

--- index.php
+++ index.php
@@ -41,6 +41,36 @@ get_header(); ?>
           </div>
       </div>
 
+      <div class="row interior-pages">
+          <div class="one-third column">
+              <?php dynamic_sidebar('bottom-left-topic-resource'); ?>
+          </div>
+
+          <div class="one-third column">
+              <?php dynamic_sidebar('bottom-middle-topic-resource'); ?>
+          </div>
+
+          <div class="one-third column">
+              <?php dynamic_sidebar('bottom-right-topic-resource'); ?>
+          </div>
+      </div>
+
+      <div class="row">
+      <?php
+      if(have_posts()){
+          while(have_posts()){
+              the_post();?>
+              <div class="one-half column posts">
+                  <?php the_post_thumbnail('thumb'); ?>
+                  <p><?php echo "Published: " . get_the_date(); ?></p>
+                  <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                  <?php the_excerpt(); ?>
+              </div>
+            <?php        }// this ends the while loop
+      }// this ends the if statement
+  ?>
     </div>
+
+</div>
 
 <?php get_footer(); ?>
