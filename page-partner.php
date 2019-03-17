de4bde26df811418837b657209509293404fdd8e
Sydney Edwards <sydneyedwards@sydneys-air-2.attlocal.net>
2019-03-16 23:30:24 -0400

    Add widgets to partner page

---
+++ page-partner.php
@@ -0,0 +1,65 @@
+<?php /*Template Name: Partner Page Template*/?>
+
+<?php get_header(); ?>
+
+<div class="container">
+
+<!-- Image + About Info Section-->
+    <div class="row">
+        <div class="one-half column">
+            <?php dynamic_sidebar('about-image'); ?>
+        </div>
+
+        <div class="one-half column">
+            <?php dynamic_sidebar('about-discover'); ?>
+        </div>
+    </div>
+
+<!--Partner Logos-->
+    <div class="row interior-pages">
+      <div class="three columns">
+        <?php dynamic_sidebar('far-left-logo'); ?>
+      </div>
+
+      <div class="three columns">
+        <?php dynamic_sidebar('left-logo'); ?>
+      </div>
+
+      <div class="three columns">
+        <?php dynamic_sidebar('right-logo'); ?>
+      </div>
+
+      <div class="three columns">
+        <?php dynamic_sidebar('far-right-logo'); ?>
+      </div>
+    </div>
+
+<!--Our Research Section-->
+      <div class="row">
+        <div class="twelve columns">
+          <?php dynamic_sidebar('vector-gen-approach'); ?>
+        </div>
+      </div>
+
+<!--Our Partner Bios Section-->
+    <div class="row interior-pages">
+
+      <div class="one-third column">
+        <?php dynamic_sidebar('left-partner-bio'); ?>
+      </div>
+
+      <div class="one-third column">
+        <?php dynamic_sidebar('right-partner-bio'); ?>
+      </div>
+    </div>
+
+<!--Ready To Join Section-->
+      <div class="row">
+        <div class="twelve columns">
+          <?php dynamic_sidebar('ready-to-join'); ?>
+        </div>
+      </div>
+
+  </div>
+
+  <?php get_footer(); ?>
