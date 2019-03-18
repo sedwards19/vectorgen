9dca593a43f08ab281ef944459c8b213962b7362
Rachel McCart <rlmccart@ufl.edu>
2019-03-18 15:07:01 -0500

    Data and information class in index

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
   <div class="row">
       <div class="twelve columns">
-          <h3 class="additional-resources-two">Data & Information</h3>
+          <h3 class="additional-resources-two">DATA & INFORMATION</h3>
       </div>
   </div>
   <!--Topics Section-->
