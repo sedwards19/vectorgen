b2128072afa45e804435303415e445fc87c5df5c
Rachel McCart <rlmccart@ufl.edu>
2019-03-20 20:37:40 -0500

    Sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,14 @@
 <div class="sidebar">
     <?php dynamic_sidebar('sidebar'); ?>
 </div>
+
+<!-- Featured Posts -->
+      <div class="row featured-posts-container">
+          <div class="one-half column">
+              <?php dynamic_sidebar('sidebar-post-one'); ?>
+          </div>
+
+          <div class="one-half column">
+              <?php dynamic_sidebar('sidebar-post-two'); ?>
+          </div>
+      </div>
