7f9a6f8313c7f5ce68858b19ba1c66b630828f14
Rachel McCart <rlmccart@ufl.edu>
2019-03-20 20:37:14 -0500

    Example blog post page.

--- single.php
+++ single.php
@@ -3,34 +3,51 @@ get_header(); ?>
 
 <div class="container">
 
-<!--Hero Image Section-->
-  <div class="row">
+<Title-->
+<div class="row">
     <div class="twelve columns">
-      <?php dynamic_sidebar('blog-hero-image'); ?>
+        <h3 class="blog-title">TITLE</h3>
+        <h3 class="blog-topic">TITLE</h3>
     </div>
-  </div>
+</div>
 
+
+<!-- SIDEBAR -->
+
+    <div class="blog-topics-container">
+        <?php dynamic_sidebar('sidebar'); ?>
+    </div>
+
+
+<!-- RECOMMENDED FOR YOU -->
+
+<div class="row">
+    <div class="twelve columns">
+        <h3 class="blog-title">RECOMMENDED FOR YOU</h3>
+    </div>
+</div>
   <!--Blog Topics Section-->
       <div class="blog-topics-container">
-          <div class="square">
-            <div class="content">
-              <?php dynamic_sidebar('blog-left-topic-resource'); ?>
+          <div class="blog-square">
+            <div class="blog-content">
+              <?php dynamic_sidebar('blog-left-topic'); ?>
             </div>
           </div>
 
-          <div class="square">
-            <div class="content">
-              <?php dynamic_sidebar('blog-middle-topic-resource'); ?>
+          <div class="blog-square">
+            <div class="blog-content">
+              <?php dynamic_sidebar('blog-middle-topic'); ?>
             </div>
           </div>
 
-          <div class="square">
-            <div class="content">
-              <?php dynamic_sidebar('blog-right-topic-resource'); ?>
+          <div class="blog-square">
+            <div class="blog-content">
+              <?php dynamic_sidebar('blog-right-topic'); ?>
             </div>
           </div>
       </div>
 
 
-<?php get_sidebar(); ?>
+
+
 <?php get_footer(); ?>
