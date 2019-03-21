3017b98adff44f500d253bc4d023b535f75e0d55
Rachel McCart <rlmccart@ufl.edu>
2019-03-20 20:12:41 -0500

    Blog post

--- single.php
+++ single.php
@@ -1,4 +1,36 @@
 <?php /*Template Name: Example Blog Post*/
 get_header(); ?>
 
+<div class="container">
+
+<!--Hero Image Section-->
+  <div class="row">
+    <div class="twelve columns">
+      <?php dynamic_sidebar('blog-hero-image'); ?>
+    </div>
+  </div>
+
+  <!--Blog Topics Section-->
+      <div class="blog-topics-container">
+          <div class="square">
+            <div class="content">
+              <?php dynamic_sidebar('blog-left-topic-resource'); ?>
+            </div>
+          </div>
+
+          <div class="square">
+            <div class="content">
+              <?php dynamic_sidebar('blog-middle-topic-resource'); ?>
+            </div>
+          </div>
+
+          <div class="square">
+            <div class="content">
+              <?php dynamic_sidebar('blog-right-topic-resource'); ?>
+            </div>
+          </div>
+      </div>
+
+
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
