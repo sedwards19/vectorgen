fb7d6dcf204acb965312652fb560ec3cc4019053
Rachel McCart <rlmccart@ufl.edu>
2019-03-25 16:54:38 -0500

    single blog post page

--- single.php
+++ single.php
@@ -15,33 +15,26 @@ get_header(); ?>
     </div>
 </div>
 
-
-<!-- RECOMMENDED FOR YOU -->
-
-<div class="row">
-    <div class="twelve columns">
-        <h3 class="blog-title">RECOMMENDED FOR YOU</h3>
-    </div>
-</div>
   <!--Blog Topics Section-->
-      <div class="row blog-topics">
-            <div class="one-third column blog">
-              <?php dynamic_sidebar('blog-left-topic'); ?>
-            </div>
+  <div class="row">
+      <div class="twelve columns">
+          <h3 class="blog-title">RECOMMENDED FOR YOU</h3>
+      </div>
+  </div>
 
-
-            <div class="one-third column blog">
-              <?php dynamic_sidebar('blog-middle-topic'); ?>
-            </div>
-
-
-
-            <div class="one-third column blog">
-              <?php dynamic_sidebar('blog-right-topic'); ?>
-            </div>
-
+  <div class="row interior-pages resource">
+      <div class="one-third column resource">
+          <?php dynamic_sidebar('resource-left-topic'); ?>
       </div>
 
+      <div class="one-third column resource">
+          <?php dynamic_sidebar('resource-middle-topic'); ?>
+      </div>
+
+      <div class="one-third column resource">
+          <?php dynamic_sidebar('resource-right-topic'); ?>
+      </div>
+  </div>
 
 
 
