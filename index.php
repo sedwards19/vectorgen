87e6172b81e324bf925870989d8d5b882470ab7d
Rachel McCart <rlmccart@ufl.edu>
2019-03-25 13:23:34 -0500

    update widgets

--- index.php
+++ index.php
@@ -16,37 +16,38 @@
           <h3 class="additional-resources-two">DATA & INFORMATION</h3>
       </div>
   </div>
-  <!--Topics Section-->
-      <div class="row resource-topics">
-            <div class="one-third column resource">
-              <?php dynamic_sidebar('top-left-topic-resource'); ?>
-            </div>
 
-            <div class="one-third column resource">
-              <?php dynamic_sidebar('top-middle-topic-resource'); ?>
-            </div>
-
-            <div class="one-third column resource">
-              <?php dynamic_sidebar('top-right-topic-resource'); ?>
-            </div>
+  <div class="row interior-pages contact">
+      <div class="one-third column contact">
+          <?php dynamic_sidebar('contact-left-topic'); ?>
       </div>
 
-      <div class="row resource-topics-two">
-            <div class="one-third column resource">
-              <?php dynamic_sidebar('bottom-left-topic-resource'); ?>
-            </div>
-
-            <div class="one-third column resource">
-              <?php dynamic_sidebar('bottom-middle-topic-resource'); ?>
-            </div>
-
-            <div class="one-third column resource">
-              <?php dynamic_sidebar('bottom-right-topic-resource'); ?>
-            </div>
+      <div class="one-third column contact">
+          <?php dynamic_sidebar('contact-middle-topic'); ?>
       </div>
+
+      <div class="one-third column contact">
+          <?php dynamic_sidebar('contact-right-topic'); ?>
+      </div>
+  </div>
+
+  <div class="row interior-pages contact">
+      <div class="one-third column contact">
+          <?php dynamic_sidebar('contact-left-topic'); ?>
+      </div>
+
+      <div class="one-third column contact">
+          <?php dynamic_sidebar('contact-middle-topic'); ?>
+      </div>
+
+      <div class="one-third column contact">
+          <?php dynamic_sidebar('contact-right-topic'); ?>
+      </div>
+  </div>
+
 
 <!-- Featured Posts -->
-  <!--    <div class="row blog-topics-container"> -->
+    <div class="row blog-topics-container">
           <div class="one-half column resource">
               <?php dynamic_sidebar('featured-post-one'); ?>
           </div>
@@ -54,6 +55,7 @@
           <div class="one-half column resource">
               <?php dynamic_sidebar('featured-post-two'); ?>
           </div>
+    </div>
 </div>
 
 <?php get_footer(); ?>
