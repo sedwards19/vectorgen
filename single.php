229f93d4d8d915e45877cbef94a05af8b85540d3
Rachel McCart <rlmccart@ufl.edu>
2019-03-26 08:13:49 -0500

    sidebar posts

--- single.php
+++ single.php
@@ -11,6 +11,28 @@ get_header(); ?>
         <div class="one-third column sidebar single">
           <!-- sidebar -->
             <?php get_sidebar(); ?>
+            <?php
+                if(have_posts()){
+                    while(have_posts()){
+                        the_post();?>
+                        <div class="one-half column one">
+                            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                            <?php the_post_thumbnail('thumb'); ?>
+                            <?php the_excerpt(); ?>
+                           <p><?php echo "Published: " . get_the_date(); ?></p>
+                           <p><?php echo "Article written by: " . get_the_author(); ?></p>
+                        </div>
+
+                        <div class="one-half column two">
+                            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                            <?php the_post_thumbnail('thumb'); ?>
+                            <?php the_excerpt(); ?>
+                           <p><?php echo "Published: " . get_the_date(); ?></p>
+                           <p><?php echo "Article written by: " . get_the_author(); ?></p>
+                        </div>
+        <?php        }// this ends the while loop
+                }// this ends the if statement
+            ?>
         </div>
     </div>
   </div>
